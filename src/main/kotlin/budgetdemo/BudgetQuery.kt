2b8ad82a340f631e7a5eb0b1ea70698c56bf0c3e
Julian Shen <julian.shen@linecorp.com>
2018-06-28 13:58:16 +0800

    Extract methods

--- src/main/kotlin/budgetdemo/BudgetQuery.kt
+++ src/main/kotlin/budgetdemo/BudgetQuery.kt
@@ -10,13 +10,17 @@ class BudgetQuery(val budgetRepo: BudgetRepository) {
             throw InvalidDateException()
         }
 
-        val allBudgets = budgetRepo.findAll().map { it.yearMonth to it.amount }.toMap()
-        val numOfDays = ChronoUnit.DAYS.between(from, to)
+        return getTotalAmount(from, to, getAllBudgetsAsMap())
+    }
 
+    private fun getAllBudgetsAsMap() = budgetRepo.findAll().map { it.yearMonth to it.amount }.toMap()
+
+    private inline fun getTotalAmount(from: LocalDate, to: LocalDate, allBudgets: Map<YearMonth, Int>): Int {
+        val numOfDays = ChronoUnit.DAYS.between(from, to)
         return listOf(0..numOfDays).flatMap { it }.map {
             YearMonth.from(from.plusDays(it))
         }.groupingBy { it }.eachCount().map {
-            (allBudgets[it.key]?:0) * it.value / it.key.lengthOfMonth()
+            (allBudgets[it.key] ?: 0) * it.value / it.key.lengthOfMonth()
         }.sum()
     }
 }
